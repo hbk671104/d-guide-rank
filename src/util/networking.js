f88708cba50b4fcf92b6bb7ccc86a30e22de5f4c
hbk671104 <hbk671104@gmail.com>
2018-05-19 22:19:11 +0800

    base uri

--- src/util/networking.js
+++ src/util/networking.js
@@ -1,7 +1,7 @@
 import Frisbee from 'frisbee'
 
 const frisbee = new Frisbee({
-	baseURI: 'http://localhost:8080',
+	baseURI: 'http://47.75.65.198:3000/api/',
 	headers: {
 		Accept: 'application/json',
 		'Content-Type': 'application/json'
